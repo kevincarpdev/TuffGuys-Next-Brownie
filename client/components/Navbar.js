4820269a1d7a44a8d016bde0fb813fde6510c478
Kevin Carpenter <carpenter.kevin@gmail.com>
2022-07-17 15:54:52 -0700

    Cleanup nav and functionality

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,23 +1,24 @@
+// components/Navbar/index.js
 import React from "react";
 import Link from 'next/link'
 import Image from 'next/image'
-import Logo from "../public/img/logo.png";
 
-export default function Navbar(props) {
-  const { signer, balance, address, refreshAddress } = props;
+// This returns a **readable** wallet address
+const formatAddress = (address) =>
+  address.slice(0, 5) + '...' + address.slice(38)
 
+export default function Navbar({ address, connectWallet, theme, setTheme }) {
   return (
-    <div className={"menuBar"}>
-      <Link className="logo"  href="/">
-          <Image
-            src={Logo}
-            alt={"Tuff Guys"}
-            quality="85"
-            layout="intrinsic"
-            loading="lazy"
-          />
-				</Link>
-				<div className={"mainNav"}>
+    <div className="menuBar">
+        {/* logo */}
+        <div className="hidden cursor-pointer sm:inline-flex">
+          <Link href="/">
+            <a>
+              <Image src="/logo.png" width={112} height={112} alt={"Tuff Guys"} quality="85" loading="lazy" />
+            </a>
+          </Link>
+        </div>
+        <div className={"mainNav"}>
           <ul className="mainNavList">
             <Link href="/fuse">
               Fuse
@@ -37,21 +38,23 @@ export default function Navbar(props) {
             <Link href="/">
               Contact
             </Link>
-					</ul>
-				</div>
-				<div className="wallet">
-          {/* {balance && (
-            <p>
-              Balance: {balance} ETH
+          </ul>
+        </div>
+         
+        {/* navigation & user's address */}
+        {address && (
+          <div className="flex items-center space-x-3">
+            <Link href="/">
+              <a className="hover:underline">My NFTs</a>
+            </Link>
+            <Link href="/create">
+              <a className="hover:underline">Create</a>
+            </Link>
+            <p className="rounded-md bg-green-400 py-2 px-3 text-white">
+              {formatAddress(address)}
             </p>
-          )} */}
-          {address && (
-           <button className="button" onClick={refreshAddress}>
-						 {address.slice(0, 6) + '...' + address.slice(36)}
-					</button>
-          )}
-					
-				</div>
-			</div>
-  );
+          </div>
+        )}
+    </div>
+  )
 }
